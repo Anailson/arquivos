6c826f3d92603ef48dc4104ce953614e2ba09379
anailson ribeiro <arsantos315@gmail.com>
2021-05-06 16:13:48 -0300

    Escrevendo em arquivo de TXT

--- src/arquivos/Arquivos.java
+++ src/arquivos/Arquivos.java
@@ -1,9 +1,35 @@
 package arquivos;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+ * Anailson - Lendo arquivos de textos
+ */
 public class Arquivos {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		File arquivo = new File("C:\\eclipse-workspace\\modulo-arquivos\\src\\arquivos\\teste-arquivo.txt");
+		
+		if (!arquivo.exists()) {//caso não tenha o arquivo [teste-arquivo.tx] o mesmo será criado
+			arquivo.createNewFile();
+		}
+		
+		FileWriter escrever_no_arquivo = new FileWriter(arquivo);
+		
+		for (int i = 0; i <=10; i++) {
+			escrever_no_arquivo.write("Texto da minha linha" + i + "\n");
+		}
+		
+		//escrever_no_arquivo.write("Meu texto do arquivo");
+		//escrever_no_arquivo.write("\n");
+		//escrever_no_arquivo.write("Minha segunda linha");
+		
+		
+		escrever_no_arquivo.flush();
+		escrever_no_arquivo.close();
 
 	}
 
---
+++ src/arquivos/Arquivos2.java
@@ -0,0 +1,59 @@
+package arquivos;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Anailson - Lendo arquivos de textos
+ */
+public class Arquivos2 {
+
+	public static void main(String[] args) throws IOException {
+		
+		Pessoa pessoa1 = new Pessoa();
+		pessoa1.setEmail("pessoa1@gmail.com");
+		pessoa1.setIdade(50);
+		pessoa1.setNome("Usuario Teste Lista ");
+			
+
+		Pessoa pessoa2 = new Pessoa();
+		pessoa2.setEmail("pessoa2@gmail.com");
+		pessoa2.setIdade(30);
+		pessoa2.setNome("Pessoa 2 Teste Lista ");
+		
+
+		Pessoa pessoa3 = new Pessoa();
+		pessoa3.setEmail("pessoa3@gmail.com");
+		pessoa3.setIdade(15);
+		pessoa3.setNome("Usuario3 Teste Lista ");
+		
+		//A LISTA PODE VIR DE ALGUM BANCO DE DADOS OU FONTE DE DADOS
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		pessoas.add(pessoa1);
+		pessoas.add(pessoa2);
+		pessoas.add(pessoa3);
+		
+		
+		File arquivo = new File("C:\\eclipse-workspace\\modulo-arquivos\\src\\arquivos\\listapessoas-arquivo.txt");
+		
+		if (!arquivo.exists()) {//caso não tenha o arquivo [teste-arquivo.tx] o mesmo será criado
+			arquivo.createNewFile();
+		}
+		
+		FileWriter escrever_no_arquivo = new FileWriter(arquivo);
+		
+		for (Pessoa p : pessoas) {
+			escrever_no_arquivo.write(p.getNome() +
+					";" + p.getEmail() + ";" +
+					p.getIdade()+ "\n");
+		}
+		
+		escrever_no_arquivo.flush();
+		escrever_no_arquivo.close();
+
+	}
+
+}
